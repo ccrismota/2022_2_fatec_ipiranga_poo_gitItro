1be4cf41bb0f0fa67a6bf63b5dbbf499891dd104
Cícero Cristiano Mota <ccrismota@gmail.com>
2022-09-01 16:37:54 -0300

    Calculo de salario efetuado de acordo com o tipo

--- Empregado.java
+++ Empregado.java
@@ -1,6 +1,20 @@
 public class Empregado{
     private String nome;
     private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+
+    public double calcularSalario(){
+        if(tipo == 1)
+        return salario;
+        if(tipo == 2)
+        return salario + salario * comissao;
+        if(tipo == 3)
+        return salario + bonus;
+      return 0;
+    }
 
     public int getIdade() {
         return idade;
@@ -17,5 +31,14 @@ public class Empregado{
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
+
+    //NOTAS: 
+
+    //O stado do objeto é constituido dos valores de suas variaveis, no caso de "p" o seu estado é energia=5, fome=5, sono=5;
+    //Neste caso não foi encapsulado as variaveis para que se tenha acesso pq não quer que mude os valores inicias do jogo;
+
+    //COMMIT É TORNAR A VERSÃO PERMANENT
+
+    //UNTRACKED = SEM CONTROLE DE VERSÃO
+    //O 'A' representa a passagem do untracked para o Tracked e o verde significa que está stadge;
 }
